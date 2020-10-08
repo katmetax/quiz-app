f3535e78d7ec0a0d7052164b8b460af799e95cfc
Katerina Metaxopoulou <kat@katmetax.com>
2020-10-09 00:42:37 +0100

    Added back/next btns and passing question number to store

--- src/components/quizComponent/index.tsx
+++ src/components/quizComponent/index.tsx
@@ -17,23 +17,36 @@ interface questionData {
 const QuizComponent = ( { questionData }: quizComponentProps ) => {
     const dispatch = useDispatch();
     const [currentIndex, setIndex] =  useState(-1);
+    const [pageNumber, setPageNumber] = useState(0);
 
-    const clickHandler = (question: string, answer: string, index: number) => {
-        selectedAnswer(question, answer);
-        setIndex(index);
+    const clickHandler = (question: string, answer: string, answerIndex: number, questionNo: number) => {
+        selectedAnswer(question, answer, questionNo);
+        setIndex(answerIndex);
     }
 
-    const selectedAnswer: any = (question: string, answer: string) => {
+    const selectedAnswer: any = (question: string, answer: string, questionNo: number) => {
         return dispatch(saveAnswer({ 
             question, 
-            answer
+            answer,
+            questionNo
         }))
+    };
+
+    const nextBtnClick = () => {
+        setPageNumber(pageNumber + 1);
+        setIndex(-1);
+    };
+
+    const backBtnClick = () => {
+        setPageNumber(pageNumber - 1);
+        setIndex(-1);
     };
 
     return (
         <div className="quiz-container" data-testid="question-root">
             {
-                questionData.map((item: any, i: any) => {
+                questionData.map((item: any, i: number) => {
+                    if (i === pageNumber) {
                     return (
                         <>
                         <h3>{i + 1}. {item.question}</h3>
@@ -41,13 +54,18 @@ const QuizComponent = ( { questionData }: quizComponentProps ) => {
                         { 
                             item.answers.map((answer: any, index: any) => {
                                 return (
-                                <li key={index} className={currentIndex === index ? 'active' : ''} onClick={() => clickHandler(item.question, answer, index)} data-testid="answers-option">{answer}</li>
+                                <li key={index} className={currentIndex === index ? 'active' : ''} onClick={() => clickHandler(item.question, answer, index, i)} data-testid="answers-option">{answer}</li>
                                 )
                             })
                         }
                         </ul>
+                        <button onClick={() => backBtnClick()}>Back</button>
+                        <button onClick={() => nextBtnClick()}>Next</button>
                         </>
                     )
+                    }
+
+                    return null;
                 })
             }
         </div>
